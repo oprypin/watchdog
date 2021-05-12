274106229604c7c90916cb6af636b45e1d5d65e5
Oleh Prypin <oleh@pryp.in>
2021-05-12 21:17:01 +0200

    Less sleeping

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from watchdog.events import FileSystemEventHandler
 
 with open("a.txt", "w") as f:
     f.write("aa\n")
-time.sleep(0.1)
+time.sleep(0.05)
 
 event_handler = FileSystemEventHandler()
 event_handler.on_any_event = print
@@ -15,41 +15,41 @@ print('--------')
 observer = Observer()
 observer.schedule(event_handler, ".", recursive=True)
 observer.start()
-time.sleep(0.1)
+time.sleep(0.05)
 
 print('[overwriting a file]')
 f = open("a.txt", "w")
 f.write("bb\n")
 f.close()
 
-time.sleep(1)
+time.sleep(0.05)
 observer.stop()
 observer.join()
 
 f = open("a.txt", "w")
 
-time.sleep(0.1)
+time.sleep(0.05)
 
 print('--------')
 
 observer = Observer()
 observer.schedule(event_handler, ".")
 observer.start()
-time.sleep(0.1)
+time.sleep(0.05)
 
 print('[appending to a file]')
 f.write("cc\n")
 f.flush()
 
-time.sleep(0.1)
+time.sleep(0.05)
 
 print('[appending to a file]')
 f.write("dd\n")
 f.flush()
 
-time.sleep(0.1)
+time.sleep(0.05)
 observer.stop()
 observer.join()
-time.sleep(0.1)
+time.sleep(0.05)
 
 f.close()
